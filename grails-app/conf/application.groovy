a2a45fcddad8ac3f69a80b7a7750a0ebb44ee508
ewesolowski <emil.wesolowski@blocks.tools>
2017-01-08 21:17:55 +0100

    proper configuration for used plugins

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -9,3 +9,34 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
         [pattern: '/**/img/**',   access: ['permitAll']],
         [pattern: '/**/favicon.ico',   access: ['permitAll']]
 ]
+
+grails.plugin.springsecurity.roleHierarchy = '''
+   ROLE_SUPER_USER > ROLE_ADMIN
+   ROLE_ADMIN > ROLE_USER
+'''
+
+grails {
+    plugin {
+        auditLog {
+            auditDomainClassName = 'blocks.BlocksAuditLogEvent'
+            verbose = true // verbosely log all changed values to db
+            logIds = true  // log db-ids of associated objects.
+            largeValueColumnTypes = true // use large column db types for oldValue/newValue.
+            TRUNCATE_LENGTH = 1000
+            cacheDisabled = true
+            logFullClassName = true
+            replacementPatterns = ["":""]
+            actorClosure = { request, session ->
+                // SpringSecurity Core 1.1.2
+                if (request.applicationContext.springSecurityService.principal instanceof java.lang.String){
+                    return request.applicationContext.springSecurityService.principal
+                }
+                def username = request.applicationContext.springSecurityService.principal?.username
+                if (SpringSecurityUtils.isSwitched()){
+                    username = SpringSecurityUtils.switchedUserOriginalUsername+" AS "+username
+                }
+                return username
+            }
+        }
+    }
+}
